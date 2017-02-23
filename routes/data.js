50102b42c32abd60608d12b941302b79af9f1f1d
Tom <thomdw@gmail.com>
2017-02-23 21:29:19 +0800

    Created routes to try things out.

--- routes/data.js
+++ routes/data.js
@@ -1,1 +1,18 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+var response = {
+    documentation: 'https://github.com/walkertd/roomba-follower'
+};
+
+router.get('/', function(req, res) {
+    res.send(response);
+});
+
+router.get('/hallo', function(req, res) {
+    res.send('oh no!');
+})
+
+module.exports = router;
 
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,18 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
 
+var response = {
+    documentation: 'https://github.com/walkertd/roomba-follower'
+};
+
+router.get('/', function(req, res) {
+    res.send(response);
+});
+
+router.get('/help', function(req, res) {
+    response.confused = "I don't really know what I'm doing.  This is a learning experience.";
+    res.send(response);
+});
+
+module.exports = router;
