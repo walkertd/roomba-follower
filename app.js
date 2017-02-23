8d39251bd283054cd9f1919b4438e4b59a3a8f59
Tom <thomdw@gmail.com>
2017-02-23 21:15:37 +0800

    Added basic error handling.  They look super awesome with no CSS....

--- app.js
+++ app.js
@@ -3,30 +3,58 @@
 const chalk = require('chalk');
 const debug = require('debug')('roombafollower');
 
+var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
+//var cookieParser = require('cookie-parser');
+var nunjucks = require('nunjucks');
+
+// Routes
+var indexRoute = require('./routes/index');
+var dataRoute = require('./routes/data');
 
 
+// Configure Express with Nunjucks
 var app = express();
 
-app.use(express.static('./public'));
-// TODO - make some public pages
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app,
+	watch: true
+})
+app.use(bodyParser.json());
 
-// TODO - wtf are views?
+// TODO - add middleware for cookies? (cookieParser)
+// TODO - add authentication middleware? (passport)
 
-// TODO - auth middleware
+app.use(express.static(path.join(__dirname, '/public')));
 
-// TODO - ssl support
+app.use('/', indexRoute);
+app.use('/data', dataRoute);
 
-// TODO - error handling middleware
-
-// TODO - routing
-// TODO - make hello world route
-// TODO - make route for showing all recordings in table
 
 // TODO - move index.js to bin/recording.js
 // TODO - make recording entry in packages.json (node.js./bin/recording.js)
 // TODO - make start script for recording
 
 
+// Error handling middleware
+
+// All other routes handled by this point - throw a 404.
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.format({
+		html: function() {
+			res.render(path.join('error','404.html'), { url: req.url});
+		},
+		json: function() {
+			res.json({error: 'Not found'});
+		},
+		default: function() {
+			res.type('txt').send('Not found.');
+		}
+	});
+});
 
 module.exports = app;
